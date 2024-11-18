04d9bbc0a32daa094a9871e068d2baef66278076
John McBride <john.mcbride@mac.com>
2024-11-18 11:18:02 -0600

    Initial Commit

---
+++ ZeroBeta4Tests/ZeroBeta4Tests.swift
@@ -0,0 +1,16 @@
+//
+//  ZeroBeta4Tests.swift
+//  ZeroBeta4Tests
+//
+//  Created by John McBride on 11/18/24.
+//
+
+import Testing
+
+struct ZeroBeta4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
